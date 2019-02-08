10357e8db4fd0dd340db5de3adcd7930c5873c03
MacStudent <macstudent@126-iMac26.local>
2019-02-07 21:03:50 -0500

    day 1st

--- day1/main.swift
+++ day1/main.swift
@@ -30,10 +30,73 @@ if a>b {
 }else{
     print("b is larger")
 }
-for  i in 1...10
+var n:Int = 10
+var t:Int=5
+/*for  i in 1...n
 {
     print(i ,terminator: " ")
 }
 
+//var j:Int = 5
+//for  i in 1..<j
+//{
+ //   print(i )
+//}
 
+
+for  i in stride(from: n, to: 0, by: -1)
+{
+    print(i )
+}
+
+for i in 1...n
+{
+    
+    print("\(5) * \(i) = \(5*i)")
+}
+for i in 1...n
+{
+    if(i==5)
+    {
+    for a in 1...n
+    {
+    print("\(i) * \(a) = \(i*a)")
+    
+        }
+        
+    }
+    
+}*/
+
+for i in 1...n
+{
+    var t=0
+    
+        for a in 1...i
+        {
+            t=t+n
+    }
+            print("\(n) * \(i) = \(t)")
+            
+        }
+        
+   // var _ =100
+//print(_)
+    var p = (100,200,300)
+print(p.0)
+var q = (a:10,b:20)
+print(q.a)
+
+var r = (a:10,(100,100))
+print(r.1.0)
+
+var m = (a: 10, x: (100,100),(1000,2000))
+print(m.x.0)
+
+var l = p
+var (x0,x1,x2)=p
+print(x0,x1,x2)
+
+var (_, xn) = q
+print(x1)
 
