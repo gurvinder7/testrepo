3bda0a7a981f3e746a876fc9e2df1f61703efec1
MacStudent <macstudent@126-iMac26.local>
2019-02-07 19:30:55 -0500

    Initial Commit

---
+++ day1/main.swift
@@ -0,0 +1,39 @@
+//
+//  main.swift
+//  day1
+//
+//  Created by MacStudent on 2019-02-07.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+//import UIKit
+
+var number1 = 10
+var number2 = 20
+var sum=number1+number2
+print("sum is" , sum)
+print("sum is this \(sum)")
+print("\(number1)+\(number2)=\(sum)")
+
+var a: Int=0
+a=20
+var b: Int=0
+b=24
+var c: Int=0
+c=a+b
+print("sum \(c)")
+
+if a>b {
+    print("a is larger")
+}else{
+    print("b is larger")
+}
+for  i in 1...10
+{
+    print(i ,terminator: " ")
+}
+
+
+
